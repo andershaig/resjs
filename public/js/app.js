a6d9ccd9c2497a547ca80b77b186958675c22680
Anders Haig <andershaig@gmail.com>
2012-01-07 21:35:16 -0800

    Trying out pre-compiled templates

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 $(function() {
-	var template = Handlebars.template.resources;
+	var template = Handlebars.templates['resources'];
 	$("#content").replaceWith(template(res_data));
 	$("#wrapper").dashboard({ columns: 3 });
 });
